f3cb011511850bcee19963f14b1e0cbbe06991dc
ionic-project <carlped417@gmail.com>
2023-10-02 12:46:04 -0500

    update nav file

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -16,7 +16,16 @@ const Navigation = () => {
     <>
       <Navbar expand="lg" bg="light">
         <Container fluid>
-          <Navbar.Brand href="#home">ETM</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="logo.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            ETM
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="justify-content-end flex-grow-1 pe-3 color-white">
