9e8b4e46c029ef5a74e9f2d37d7102383cbe42c9
Joseph V. Miloscia <jmiloscia41@gmail.com>
2023-10-04 16:15:53 -0500

    Front-End push

--- src/App.js
+++ src/App.js
@@ -8,22 +8,25 @@ import Profile from "./components/Profile";
 import About from "./components/About";
 import "./styles/App.css";
 import RssFeed from "./components/RssFeed";
+import { UserProvider } from "./contexts/UserProvider";
 
 function App() {
   return (
     <div className="wrap">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigation />}>
-            <Route index element={<Home />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/newsfeed" element={<RssFeed />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/about" element={<About />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <UserProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navigation />}>
+              <Route index element={<Home />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/newsfeed" element={<RssFeed />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/about" element={<About />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </UserProvider>
     </div>
   );
 }
---
+++ src/contexts/UserProvider.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import UserContext from "./UserContext";
+
+export const UserProvider = (props) => {
+  const baseUrl = "http://localhost:3000/api/users";
+
+  function createUser(username, password, fullname, email, city, state) {
+    let user = {
+      username,
+      password,
+      fullname,
+      email,
+      city,
+      state,
+    };
+
+    return axios.post(baseUrl, user).then((response) => {
+      return new Promise((resolve) => resolve(response.data));
+    });
+  }
+
+  function signInUser(username, password) {
+    let user = { username, password };
+
+    return axios.post(`${baseUrl}/login`, user).then((response) => {
+      localStorage.setItem("myPostToken", response.data.token);
+      return new Promise((resolve) => resolve(response.data));
+    });
+  }
+
+  function getUserProfile(_id) {
+    return axios.get(baseUrl + _id).then((response) => {
+      return new Promise((resolve) => resolve(response.data));
+    });
+  }
+
+  return (
+    <UserContext.Provider
+      value={{
+        createUser,
+        signInUser,
+        getUserProfile,
+      }}
+    >
+      {props.children}
+    </UserContext.Provider>
+  );
+};
