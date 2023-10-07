688e14659b75fa2d284ac6018e3bd5cf9f433ada
Ebuka Mefor <ebukamefor@gmail.com>
2023-10-07 13:58:58 -0400

    working on profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,112 @@
 import React from "react";
+import "../styles/Profile.css";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <section className="profile-section">
+      <div className="row">
+        <div className="col-lg-4">
+          <div className="card mb-4">
+            <div className="card-body text-center">
+              <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
+                alt="avatar"
+                className="rounded-circle img-fluid"
+                style={{ width: "150px" }}
+              />
+              <h5 className="my-3">John Smith</h5>
+              <p className="text-muted mb-1">Full Stack Developer</p>
+              <p className="text-muted mb-4">Grand Rapids, MI</p>
+              <div className="d-flex justify-content-center mb-2">
+                <button
+                  type="button"
+                  className="btn btn-primary btn-lg btn-block"
+                >
+                  QAK
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-lg-8">
+          <div className="card mb-4">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-sm-3">
+                  <p className="mb-0">User_ID</p>
+                </div>
+                <div className="col-sm-9">
+                  <p className="text-muted mb-0">123456</p>
+                </div>
+              </div>
+              <hr />
+              <div className="row">
+                <div className="col-sm-3">
+                  <p className="mb-0">Full Name</p>
+                </div>
+                <div className="col-sm-9">
+                  <p className="text-muted mb-0">Johnatan Smith</p>
+                </div>
+              </div>
+              <hr />
+              <div className="row">
+                <div className="col-sm-3">
+                  <p className="mb-0">Email</p>
+                </div>
+                <div className="col-sm-9">
+                  <p className="text-muted mb-0">example@example.com</p>
+                </div>
+              </div>
+              <hr />
+              <div className="row">
+                <div className="col-sm-3">
+                  <p className="mb-0">Phone</p>
+                </div>
+                <div className="col-sm-9">
+                  <p className="text-muted mb-0">(097) 234-5678</p>
+                </div>
+                <hr />
+                <div className="row">
+                  <div className="col-sm-3">
+                    <p className="mb-0">City</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text-muted mb-0">Grand Rapids</p>
+                  </div>
+                  <hr />
+                  <div className="row">
+                    <div className="col-sm-3">
+                      <p className="mb-0">State</p>
+                    </div>
+                    <div className="col-sm-9">
+                      <p className="text-muted mb-0">Michigan</p>
+                    </div>
+                    <hr />
+                    <div className="row">
+                      <div className="col-sm-3">
+                        <p className="mb-0">Member-Since</p>
+                      </div>
+                      <div className="col-sm-9">
+                        <p className="text-muted mb-0">10/06/2023</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <section>
+        <div class="row">
+          <div class="col-8">Latest Content</div>
+          <div class="col-4">Connections</div>
+        </div>
+      </section>
+    </section>
+  );
 };
 
 export default Profile;
