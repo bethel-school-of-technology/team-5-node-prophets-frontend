c251cc9e6bcaaa7fa2b67787a2ff2c0a0593b64c
ionic-project <carlped417@gmail.com>
2023-10-22 14:39:43 -0500

    updated qak.js

--- src/components/Qak.js
+++ src/components/Qak.js
@@ -1,4 +1,4 @@
-import React, { useContext, useContext } from "react";
+import React, { useContext, useState } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import QakContext from "../contexts/QakContext";
 import { Link, useNavigate, useParams } from "react-router-dom";
@@ -49,48 +49,6 @@ const Qak = ({ user }) => {
   }
 
   const [filter, setFilter] = useState("7"); // Initialize the filter as "within 7 days"
-const Qak = ({ user }) => {
-  let params = useParams();
-  let navigate = useNavigate();
-
-  let { deleteQak } = useContext(QakContext);
-
-  function handleDelete(qak_id) {
-    if (params.user_id !== user) {
-      return alert("You are not allowed to perform this operation").then(() => {
-        navigate("/qaks");
-      });
-    }
-    deleteQak(qak_id)
-      .then(() => {
-        navigate("/qaks");
-      })
-      .catch((error) => {
-        console.log(error);
-        alert("You need to sign in to perform this operation");
-        navigate("/signin");
-      });
-  }
-
-  function handleDelete(qak_id) {
-    if (params.user !== user) {
-      window.alert("You are not allowed to perform this operation");
-      navigate("/qaks");
-    } else {
-      const confirmDelete = window.confirm("Are you sure you want to delete?");
-      if (confirmDelete) {
-        deleteQak(qak_id)
-          .then(() => {
-            navigate("/qaks");
-          })
-          .catch((error) => {
-            console.log(error);
-            window.alert("You need to sign in to perform this operation");
-            navigate("/qaks");
-          });
-      }
-    }
-  }
 
   return (
     <QakContext.Consumer>
