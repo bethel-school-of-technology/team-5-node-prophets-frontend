a4feb6e90f31ffdfdbf45b5e0c5e9e20be9a9c2f
ionic-project <carlped417@gmail.com>
2023-11-01 19:12:17 -0500

    Jan corrections

--- src/components/Qak.js
+++ src/components/Qak.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import QakContext from "../contexts/QakContext";
 import { Link, useNavigate } from "react-router-dom";
@@ -6,14 +6,44 @@ import moment from "moment";
 import "../styles/Qak.css";
 import NewQak from "./NewQak";
 import QakReplyContext from "../contexts/QakReplyContext";
+import UserContext from "../contexts/UserContext";
 
 const Qak = ({ user }) => {
   let navigate = useNavigate();
+  const [loggedUser, setLoggedUser] = useState([]);
 
   let { deleteQak } = useContext(QakContext);
   let { deleteQakReply } = useContext(QakReplyContext);
+  let { getUserQaks } = useContext(UserContext);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    async function fetchData() {
+      try {
+        const result = await getUserQaks();
+        if (isMounted) {
+          setLoggedUser(result);
+        }
+      } catch (error) {
+        if (isMounted) {
+          if (error.response && error.response.status === 404) {
+            console.clear();
+          }
+        }
+      }
+    }
+
+    fetchData();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
   function handleDelete(qak_id) {
+    console.log(user);
+
     if (user) {
       window.alert("You are not allowed to perform this operation");
       navigate("/qaks");
@@ -168,15 +198,15 @@ const Qak = ({ user }) => {
                               style={{ display: "flex", alignItems: "center" }}
                             >
                               <div>
-                                {!user ? (
-                                  <Link to={`/noprofile/${q.user_id}`}>
-                                    <h4>{q.User.username}</h4>
-                                  </Link>
-                                ) : (
+                                {/* {!user ? ( */}
+                                <Link to={`/noprofile/${q.user_id}`}>
+                                  <h4>{q.User.username}</h4>
+                                </Link>
+                                {/* ) : (
                                   <Link to={`/profile/${user.user_id}`}>
                                     <h4>{q.User.username}</h4>
                                   </Link>
-                                )}
+                                 )} */}
                                 <p>{q.qak}</p>
                                 <div
                                   style={{
@@ -197,25 +227,29 @@ const Qak = ({ user }) => {
                                           "MM/DD/YYYY"
                                         )}`}
                                   </p>
-
-                                  <p style={{ marginLeft: "auto" }}>
-                                    <Link
-                                      to={`/qaks/${q.qak_id}/edit`}
-                                      style={{ marginRight: "10px" }}
-                                    >
-                                      Edit
-                                    </Link>
-                                    <Link
-                                      to={"#"}
-                                      onClick={handleDelete.bind(
-                                        this,
-                                        q.qak_id,
-                                        q.user_id
-                                      )}
-                                    >
-                                      Delete
-                                    </Link>
-                                  </p>
+                                  {loggedUser &&
+                                  q.user_id == loggedUser.user_id ? (
+                                    <p style={{ marginLeft: "auto" }}>
+                                      <Link
+                                        to={`/qaks/${q.qak_id}/edit`}
+                                        style={{ marginRight: "10px" }}
+                                      >
+                                        Edit
+                                      </Link>
+                                      <Link
+                                        to={"#"}
+                                        onClick={handleDelete.bind(
+                                          this,
+                                          q.qak_id,
+                                          q.user_id
+                                        )}
+                                      >
+                                        Delete
+                                      </Link>
+                                    </p>
+                                  ) : (
+                                    <></>
+                                  )}
                                 </div>
                               </div>
                             </div>
@@ -270,24 +304,30 @@ const Qak = ({ user }) => {
                                                   QakReplies.createdAt
                                                 ).format("MM/DD/YYYY")}`}
                                           </p>
-                                          <p style={{ marginLeft: "auto" }}>
-                                            <Link
-                                              to={`/qakReply/edit/${QakReplies.qakReply_id}`}
-                                              style={{ marginRight: "10px" }}
-                                            >
-                                              Edit
-                                            </Link>
-                                            <Link
-                                              to={"#"}
-                                              onClick={handleDeleteQakReply.bind(
-                                                this,
-                                                QakReplies.qakReply_id,
-                                                QakReplies.User.user_id
-                                              )}
-                                            >
-                                              Delete
-                                            </Link>
-                                          </p>
+                                          {loggedUser &&
+                                          QakReplies.User.user_id ==
+                                            loggedUser.user_id ? (
+                                            <p style={{ marginLeft: "auto" }}>
+                                              <Link
+                                                to={`/qakReply/edit/${QakReplies.qakReply_id}`}
+                                                style={{ marginRight: "10px" }}
+                                              >
+                                                Edit
+                                              </Link>
+                                              <Link
+                                                to={"#"}
+                                                onClick={handleDeleteQakReply.bind(
+                                                  this,
+                                                  QakReplies.qakReply_id,
+                                                  QakReplies.User.user_id
+                                                )}
+                                              >
+                                                Delete
+                                              </Link>
+                                            </p>
+                                          ) : (
+                                            <></>
+                                          )}
                                         </div>
                                       </div>
                                     </div>
